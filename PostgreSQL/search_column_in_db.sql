884139f485b575df1f821d408cac20dab2e873fe
Serdar CAN <98654847+ds-serdarcan@users.noreply.github.com>
2025-01-29 14:23:03 +0300

    Update search_column_in_db.sql

--- PostgreSQL/search_column_in_db.sql
+++ PostgreSQL/search_column_in_db.sql
@@ -1,4 +1,3 @@
-SELECT table_name
-FROM information_schema.tables
-WHERE table_name LIKE '%invd_item%'
-order by 1
+SELECT table_schema, table_name, column_name
+FROM information_schema.columns
+WHERE column_name like '%item_attribute_id';
